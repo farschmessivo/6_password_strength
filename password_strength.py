116b9daf3de5800a80438789ef0ffc444d546f07
Roman Ch <hello@romangagarin.com>
2017-12-12 21:05:13 +0000

    change funcs

--- password_strength.py
+++ password_strength.py
@@ -6,32 +6,25 @@ def get_password_strength(password):
     password_strength = 0
     minimum_char_amount = 6
     optimal_char_amount = 8
-    if minimum_char_amount <= len(password) < optimal_char_amount:
+    if len(password) > minimum_char_amount:
         password_strength += 2
-    elif len(password) < 6:
-        pass
     if len(password) >= optimal_char_amount:
         password_strength += 3
     for pattern in ('[a-z]', '[A-Z]', '[0-9]', '[$#@]'):
         if re.search(pattern, password):
             password_strength += 1
-
     return password_strength
 
 
 def load_data():
-
-    with open('passwordlist.txt', 'r') as f:
-        common_passwords = f.read()
-
+    with open('passwordlist.txt', 'r') as file_handler:
+        common_passwords = file_handler.read()
     return common_passwords
 
 
 def check_password_blacklist(password, common_passwords):
     in_blacklist = re.findall(common_passwords, password)
-    if in_blacklist:
-        return 0
-    else:
+    if not in_blacklist:
         return 2
 
 
