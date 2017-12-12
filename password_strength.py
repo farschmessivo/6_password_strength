854efeb626e214c7ef314724e3d46f9ff26f5ebe
Roman Ch <hello@romangagarin.com>
2017-12-12 16:36:47 +0000

    project changes

--- password_strength.py
+++ password_strength.py
@@ -1,14 +1,16 @@
-import sys
+from getpass import getpass
 import re
 
 
 def get_password_strength(password):
     password_strength = 0
-    if 6 <= len(password) < 8:
+    absolute_minimum_char = 6
+    optimal_char = 8
+    if absolute_minimum_char <= len(password) < optimal_char:
         password_strength += 2
     elif len(password) < 6:
-        password_strength += 0
-    if len(password) >= 8:
+        pass
+    if len(password) >= optimal_char:
         password_strength += 3
     for pattern in ('[a-z]', '[A-Z]', '[0-9]', '[$#@]'):
         if re.search(pattern, password):
@@ -17,7 +19,15 @@ def get_password_strength(password):
     return password_strength
 
 
-def check_password_commonality(password):
+def load_data():
+
+    with open('passwordlist.txt', 'r') as f:
+        common_passwords = f.read()
+
+    return common_passwords
+
+
+def check_password_blacklist(password, common_passwords):
     matched_pass = False
 
     with open('passwordlist.txt', 'r') as f:
@@ -33,15 +43,18 @@ def check_password_commonality(password):
         return 2
 
 
-def password_strength_sum(strength, commonality):
-    strength_sum = strength + commonality
+def password_strength_sum(strength, blacklist):
+    strength_sum = strength + blacklist
     return strength_sum
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        sys.exit("Usage: python3 password_strength.py <password>")
-    password = sys.argv[1]
+    print('Please enter your password: ')
+    password = getpass()
+    common_passwords = load_data()
+    # if len(sys.argv) == 1:
+    #     sys.exit("Usage: python3 password_strength.py <password>")
+    # password = sys.argv[1]
     strength = get_password_strength(password)
-    commonality = check_password_commonality(password)
-    password_strength_sum = password_strength_sum(strength, commonality)
+    blacklist = check_password_blacklist(password, common_passwords)
+    password_strength_sum = password_strength_sum(strength, blacklist)
     print('Your password strength score is:', password_strength_sum)
